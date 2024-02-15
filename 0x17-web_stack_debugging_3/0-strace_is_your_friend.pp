731773f88ea9f2472b783ce2f3d2fc97025f5c47
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-02-15 03:26:04 +0200

    update task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,11 @@
-# fix the server's bug
+# fix the server bug
 
-exec { 'update settings file':
+exec { 'update the settings file':
+        provider => 'shell',
         command  => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
 }
 
-exec { 'restart apache2':
+exec { 'restart apache2 service':
+        provider => 'shell',
         command  => 'service apache2 restart',
 }
